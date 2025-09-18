d223e39863fed78164ce17125cfb9f3ad9eb063a
asua <dpaniprithiv#2018pr@gmail.com>
2025-09-18 16:04:03 +0530

    First Selenium project

--- E29/src/main/java/sample/Demo1.java
+++ E29/src/main/java/sample/Demo1.java
@@ -1,9 +1,9 @@
 package sample;
 
-public class sample {
+public class Demo1 {
 
 	public static void main(String[] args) {
-	
+		
 		System.out.println("helloo");
 		System.out.println("helloo");
 		System.out.println("helloo");
