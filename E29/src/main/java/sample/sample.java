c87b03ff252dcc29bc8cff591146bb999365efbd
asua <dpaniprithiv#2018pr@gmail.com>
2025-09-18 15:28:20 +0530

    First project

---
+++ E29/src/main/java/sample/sample.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class sample {
+
+	public static void main(String[] args) {
+	
+		System.out.println("helloo");
+		System.out.println("helloo");
+		System.out.println("helloo");
+	}
+
+}
