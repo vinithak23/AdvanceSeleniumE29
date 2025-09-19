bfb9df95c6e456a6b2e9012b3d89aa3b488ca73a
asua <dpaniprithiv#2018pr@gmail.com>
2025-09-19 14:34:08 +0530

    helooo anu

---
+++ E29/src/main/java/sample/Anuska.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Anuska {
+
+	public static void main(String[] args) {
+		
+		System.out.println("helloo anu");
+		System.out.println("helloo anu");
+		System.out.println("helloo anu");
+	}
+
+}
---
+++ E29/src/main/java/sample/Virat.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Virat {
+
+	public static void main(String[] args) {
+	
+		System.out.println("helloo virat");
+		System.out.println("helloo virat");
+		System.out.println("helloo virat");
+	}
+
+}
