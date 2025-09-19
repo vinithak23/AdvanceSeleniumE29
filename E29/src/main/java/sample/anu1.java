520a35bb51bc5c52e9c3d605baae85c3c350bc5a
asua <dpaniprithiv#2018pr@gmail.com>
2025-09-19 15:04:49 +0530

    hi...anu new

---
+++ E29/src/main/java/sample/anu1.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class anu1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("helloo anu");
+		System.out.println("helloo anu");
+		System.out.println("helloo anu");
+	}
+
+}
